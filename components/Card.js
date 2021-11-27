c6dabb4336d4fa983ce9dab6eae72153b67bb270
1forh <1forhlol@gmail.com>
2021-11-26 21:12:16 -0500

    ✨ adds zoom feature

--- components/Card.js
+++ components/Card.js
@@ -1,18 +1,57 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
 import Price from './Price';
+import { Controlled as ControlledZoom } from 'react-medium-image-zoom';
+import 'react-medium-image-zoom/dist/styles.css';
+import ZoomButton from './ZoomButton';
 
 function Card({ item }) {
+  const [isZoomed, setIsZoomed] = useState(false);
+
+  const showImage = useCallback((event) => {
+    event.preventDefault();
+    setIsZoomed(true);
+  }, []);
+
+  const handleZoomChange = useCallback((shouldZoom) => {
+    setIsZoomed(shouldZoom);
+  }, []);
+
   return (
     <a href={item.url} className='group'>
-      <Image
-        src={item.image}
-        alt={item.id}
-        width={400}
-        height={400}
-        className='rounded-lg'
-      />
+      <div className='relative'>
+        <div className='absolute top-0.5 right-0.5 z-20'>
+          <ZoomButton onClick={showImage} />
+        </div>
+
+        <div className={'transition-opacity duration-150 absolute'}>
+          <ControlledZoom
+            isZoomed={isZoomed}
+            onZoomChange={handleZoomChange}
+            zoomMargin={100}
+          >
+            {isZoomed && (
+              <div>
+                <Image
+                  src={item.image}
+                  alt={item.id}
+                  width={400}
+                  height={400}
+                  className='rounded-lg'
+                />
+              </div>
+            )}
+          </ControlledZoom>
+        </div>
+
+        <Image
+          src={item.image}
+          alt={item.id}
+          width={400}
+          height={400}
+          className='absolute inset-0 z-10 rounded-lg'
+        />
+      </div>
       <p className='text-green-50'>{item.id}</p>
       {item.name && <h3 className='text-green-50'>{item.name}</h3>}
 
