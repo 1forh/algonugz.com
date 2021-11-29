0aae3439eb1981072809edfcd5369231cdeff558
1forh <1forhlol@gmail.com>
2021-11-29 05:31:54 -0500

    💄 updates date format

--- components/TheScheduleEvent.js
+++ components/TheScheduleEvent.js
@@ -6,13 +6,13 @@ function TheScheduleEvent({ event, className }) {
   return (
     <div className={className}>
       <div
-        className={`flex items-center justify-between w-full mb-4 max-w-xl  pb-4`}
+        className={`flex items-center justify-between w-full mb-4 max-w-xl pb-4`}
       >
         <div>
           <p className='mb-2 font-medium text-opacity-75 text-green-50'>
-            {dayjs(event.date).format('MM/DD/YYYY')}
+            {dayjs(event.date).format('MMMM D, YYYY')}
           </p>
-          <div className={`space-y-5 ${event.nft && 'mb-8'}`}>
+          <div className={`space-y-3 ${event.nft && 'mb-8'}`}>
             <h3 className='text-xl font-medium text-green-300'>
               {event.title}
             </h3>
